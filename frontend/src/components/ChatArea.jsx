8a0850835608a880176b3a6ac1a626d58db74065
Brian Otieno <brianotieno586@gmail.com>
2024-03-08 08:23:47 +0300

    Chat-astrophic version 2.0.0

--- frontend/src/components/ChatArea.jsx
+++ frontend/src/components/ChatArea.jsx
@@ -2,7 +2,7 @@ import "./myStyles.css";
 import React from "react";
 
 const ChatArea = () => {
-  return <div className="chatarea-container">ChatArea</div>;
+  return <div className="chatarea-container ">ChatArea</div>;
 };
 
 export default ChatArea;
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import "./myStyles.css";
+import { IconButton } from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import GroupAddIcon from "@mui/icons-material/GroupAdd";
+import NightlightIcon from "@mui/icons-material/Nightlight";
 
 const Sidebar = () => {
   return (
     <div className="sidebar-container">
-      <div className="sb-header">Header</div>
+      <div className="sb-header">
+        <IconButton>
+          <AccountCircleIcon />
+        </IconButton>
+        <IconButton>
+          <PersonAddIcon />
+        </IconButton>
+        <IconButton>
+          <GroupAddIcon />
+        </IconButton>
+        <IconButton>
+          <AddCircleIcon />
+        </IconButton>
+        <IconButton>
+          <NightlightIcon />
+        </IconButton>
+      </div>
       <div className="sb-search">Search</div>
       <div className="sb-conversations">Conversations</div>
     </div>
