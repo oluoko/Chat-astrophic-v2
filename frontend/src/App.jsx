703a713fb43b9bd28354d8b7dda3cd612b7772a0
Brian Otieno <brianotieno586@gmail.com>
2024-03-08 03:02:35 +0300

    Chat-astrophic version 2.0.0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import MainContainer from "./components/MainContainer";
 
 function App() {
-  return <>app</>;
+  return (
+    <>
+      <div className="App">
+        <h1>Welcome to our website</h1>
+        <MainContainer />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/MainContainer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./myStyles.css";
+
+const MainContainer = () => {
+  return <div className="main-container">MainContainer</div>;
+};
+
+export default MainContainer;
