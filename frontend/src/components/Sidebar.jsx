065a87ce7c1e887d92160c775bd509ef737f2cb5
Brian Otieno <brianotieno586@gmail.com>
2024-03-08 05:45:58 +0300

    Chat-astrophic version 2.0.0

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./myStyles.css";
 
 const Sidebar = () => {
-  return <div className="sidebar-container">Sidebar</div>;
+  return (
+    <div className="sidebar-container">
+      <div className="sb-header">Header</div>
+      <div className="sb-search">Search</div>
+      <div className="sb-conversations">Conversations</div>
+    </div>
+  );
 };
 
 export default Sidebar;
