85dfac5d724996ab557879b543bcbfc93c4c096e
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-14 08:13:58 +0300

    Chat-astrophic version 2.0.0

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const colors = require("colors");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 dotenv.config();
+
+const connectDb = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log("Server is Connected to Db".yellow.bold.underline);
+  } catch (error) {
+    console.log(
+      "Server is NOT connected to Databae".red.bold.underline,
+      error.message
+    );
+  }
+};
+
+connectDb();
 
 app.get("/", (req, res) => {
   res.send("API is running");
