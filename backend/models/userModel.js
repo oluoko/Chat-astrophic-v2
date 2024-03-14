ba61acd81821d299b7a8e55f8f72c7ab00d3e8a9
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-14 05:34:39 +0300

    Chat-astrophic version 2.0.0

---
+++ backend/models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userModel = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.Model("User", userModel);
+module.exports = User;
