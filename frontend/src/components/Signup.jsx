b9b50837a7d8837f6b56bd664c552171678622f6
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-14 08:24:34 +0300

    Chat-astrophic version 2.0.0

---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,48 @@
+import logo from "../assets/chatting.png";
+import React from "react";
+import "./myStyles.css";
+import Link from "react-router-dom";
+import { Button, TextField } from "@mui/material";
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="image-container">
+        <img src={logo} alt="logo" className="welcome-logo" />
+      </div>
+      <div className="login-box">
+        <p>Create An Account</p>
+
+        <TextField
+          id="outlined-basic"
+          className="login-input"
+          label="Enter Your Email"
+          type="email"
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-basic"
+          className="login-input"
+          label="Enter Your Full Name"
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-basic"
+          label="Password"
+          className="login-input"
+          type="password"
+          variant="outlined"
+        />
+        <Button className="login-input button" variant="outlined">
+          Sign up
+        </Button>
+
+        <p>
+          Already have an account ? <Link> Log in</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
