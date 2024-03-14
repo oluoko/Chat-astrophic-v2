9c91ac00625a3240270dee90bc94cc6591667afa
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-14 23:32:25 +0300

    Chat-astrophic version 2.0.0

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,9 +2,89 @@ import logo from "../assets/chatting.png";
 import React from "react";
 import { Link } from "react-router-dom";
 import "./myStyles.css";
-import { Button, TextField } from "@mui/material";
+import { Backdrop, Button, CircularProgress, TextField } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+  const [showlogin, setShowlogin] = useState(false);
+  const { data, setData } = useState({ name: "", email: "", password: "" });
+  const [loading, setLoading] = useState(false);
+  const [logInStatus, setLogInStatus] = React.useState("");
+  const [signInStatus, setSignInStatus] = React.useState("");
+
+  const navigate = useNavigate();
+  const changerHandler = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const loginHandler = async (e) => {
+    setLoading(true);
+    console.log(data);
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const response = await axios.post(
+        "http://localhost:5000/user/login/",
+        data,
+        config
+      );
+      console.log("Login : ", response);
+      setLogInStatus({ msg: "Success", key: Math.random() });
+      setLoading(false);
+      localStorage.setItem("userData", JSON.stringify(response));
+      navigate("/app/welcome");
+    } catch (error) {
+      setLogInStatus({
+        msg: "Invalid Username or Password",
+        key: Math.random(),
+      });
+    }
+
+    setLoading(false);
+  };
+
+  const signUpHandler = async () => {
+    setLoading(true);
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const response = await axios.post(
+        "http://localhost:5000/user/register/",
+        data,
+        config
+      );
+      console.log(response);
+      setSignInStatus({ msg: "Success", key: Math.random() });
+      navigate("/app/welcome");
+      localStorage.setItem("userData", JSON.stringify(response));
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      if (error.response.status === 405) {
+        setLogInStatus({
+          msg: "User with this email ID already Exists",
+          key: Math.random(),
+        });
+      }
+      if (error.response.status == 406) {
+        setLogInStatus({
+          msg: "User Name already taken, please try another one",
+          key: Math.random(),
+        });
+      }
+      setLoading(false);
+    }
+  };
   return (
     <div className="login-container">
       <div className="image-container">
