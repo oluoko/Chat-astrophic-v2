db2dce577139270387d0bacb89181a12b77102ae
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-14 05:02:22 +0300

    Chat-astrophic version 2.0.0

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
-  res.send("API is running 12345");
+  res.send("API is running");
 });
 
-app.listen(5000, console.log("Server is Running..."));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log("Server is Running..."));
