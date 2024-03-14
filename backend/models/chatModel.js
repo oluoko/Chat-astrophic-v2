d58628b3cb1878e05dc5be590cfa507c8cc6512b
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-14 05:31:50 +0300

    Chat-astrophic version 2.0.0

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -1,20 +1,28 @@
 const mongoose = require("mongoose");
 
-const chatModel = mongoose.Schema({
-  chatName: { type: String },
-  isGroupChat: { type: Boolean },
-  users: [
-    {
+const chatModel = mongoose.Schema(
+  {
+    chatName: { type: String },
+    isGroupChat: { type: Boolean },
+    users: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    groupAdmin: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
-  ],
-  latestMessage: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Message",
   },
-  groupAdmin: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-});
+  {
+    timeStamp: true,
+  }
+);
+
+const Chat = mongoose.Model("Chat", chatModel);
+module.exports = Chat;
