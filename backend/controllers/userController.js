8704a7f409711103133b67fd9333cb19c0ad232d
brianotieno586@gmail.com <brianotieno586@gmail.com>
2024-03-14 09:26:22 +0300

    Chat-astrophic version 2.0.0

---
+++ backend/controllers/userController.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const UserModel = require("../models/userModel");
+const User = require("../models/userModel");
+const expressAsyncHandler = require("express-async-handler");
+
+const loginController = () => {};
+
+const registerController = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // check for all fields
+  if (!name || !email || !password) {
+    res.send(400);
+    throw Error("All necessary fields have not been filled!");
+  }
+
+  //pre-existing user
+  const userExist = await UserModel.findOne({ email });
+  if (userExist) {
+    throw new Error("User already Exists.");
+  }
+
+  //userName already taken
+  const userNameExist = await UserModel.findOne({ name });
+  if (userNameExist) {
+    throw new Error("Username already taken.");
+  }
+
+  const user = await UserModel.create({ name, email, password });
+};
+
+module.exports = { loginController, registerController };
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const { default: mongoose } = require("mongoose");
+const userRoutes = require("./Routes/userRouters");
 
 const app = express();
 dotenv.config();
@@ -23,6 +24,7 @@ connectDb();
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+app.use("user/", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
